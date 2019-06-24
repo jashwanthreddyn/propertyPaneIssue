42984a3dbd6a114bdde8c30e8de59c6f0db108c7
Jashwanth Reddy N <33126513+jashwanthreddyn@users.noreply.github.com>
2019-06-24 15:53:56 -0400

    Add files via upload

---
+++ testWP/src/webparts/helloWorld/components/HelloWorld.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./HelloWorld.module.css");
+const styles = {
+  helloWorld: 'helloWorld_072f94bc',
+  container: 'container_072f94bc',
+  row: 'row_072f94bc',
+  column: 'column_072f94bc',
+  'ms-Grid': 'ms-Grid_072f94bc',
+  title: 'title_072f94bc',
+  subTitle: 'subTitle_072f94bc',
+  description: 'description_072f94bc',
+  button: 'button_072f94bc',
+  label: 'label_072f94bc',
+};
+
+export default styles;
+/* tslint:enable */
